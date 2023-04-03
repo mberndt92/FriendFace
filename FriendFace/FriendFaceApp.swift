7778e7894e22e3b495e800bee57adfc1cda3925a
mberndt92 <maximilian_berndt@icloud.com>
2023-04-04 08:59:47 +0900

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Maximilian Berndt on 2023/04/04.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
